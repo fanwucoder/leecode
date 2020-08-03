316feefd6c57729b2c05611660acbaaa7f3095cc
fan <254765517@qq.com>
2020-08-03 21:51:48 +0800

    按照自己的思路来的，还是很乱

--- src/back_tracking/subsets.py
+++ src/back_tracking/subsets.py
@@ -3,18 +3,28 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ret = []
         cnt = len(nums)
-
-        def backend(first, total, cur):
-            if len(cur) == total:
-                ret.append(cur[:])
-            for i in range(first, cnt):
-                cur.append(nums[i])
-                first = i + 1
-                backend(first, total, cur)
-                cur.pop()
+        res = []
 
         for i in range(cnt + 1):
-            backend(0, i, [])
-        return ret
+            path = []
+            used = [False for x in range(cnt)]
+            self.backend(i, 0, path, used, res, cnt, nums)
+        return res
+
+    def backend(self, total, begin, path, used, res, cnt, nums):
+        if len(path) == total:
+            res.append(path[:])
+            return
+        for i in range(begin, cnt):
+            if not used[i]:
+                path.append(nums[i])
+                used[i] = True
+                self.backend(total, i + 1, path, used, res, cnt, nums)
+                used[i] = False
+                path.pop()
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.subsets([1, 2, 3]))
